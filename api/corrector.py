f551574c9933af50a94e965b3dd5591243235268
Andrea Costa <gigadr3w@gmail.com>
2022-10-06 14:36:06 +0200

    name refactoring, add correction handler

--- api/corrector.py
+++ api/corrector.py
@@ -2,9 +2,9 @@ from flask_restx import Namespace, Resource, fields
 from nltk import word_tokenize
 
 from enums.correctors import CorrectorLanguageEnum
-from models.correction import CorrectionModel
+from models.corrector import CorrectionModel
 
-from nlp.corrector import Correctors
+from handlers.corrector import CorrectorHandler
 
 api = Namespace('corrector', description='Method that correct an entire sentence due to the specified languages')
 
@@ -16,27 +16,20 @@ output_model = api.model('Corrected', {
     'sentences' : fields.String(description='The correct sentence') 
 })
 
-def suggest(language:CorrectorLanguageEnum, word:str) -> str:
-    #Suggest the first most reliable term for the wrong word passed as argument 
-    try:
-        return Correctors(language).suggest(word)[0]
-    except:
-        return word
-
 @api.route('/corrector/<string:language>')
 @api.param('language', description='The language of the dictionary with which correct the sentence', enum=CorrectorLanguageEnum._member_names_)
 class CorrectorResource(Resource):
     @api.expect(input_model)
     @api.marshal_with(output_model)
     def post(self, language):
-        corrector_language = CorrectorLanguageEnum[language]
+        corrector_handler = CorrectorHandler(CorrectorLanguageEnum[language])
         sentences = api.payload['sentences']
 
         out = CorrectionModel()
         
         for sentence in sentences:
             #check if each word is semantically corrected and if not suggest a correction
-            corrected_words = [suggest(corrector_language,word) if not Correctors[corrector_language].check(word) else word for word in word_tokenize(sentence)]
+            corrected_words = [corrector_handler.Handle(word) for word in word_tokenize(sentence)]
             out.sentences.append(str.join(' ', corrected_words))
         
         return out
---
+++ handlers/corrector.py
@@ -0,0 +1,28 @@
+import logging, enchant
+from enums.correctors import CorrectorLanguageEnum
+
+Correctors = dict()
+
+def InitCorrectors():
+    for l in CorrectorLanguageEnum:
+        Correctors[l] = enchant.Dict(l.value)
+        logging.debug('Enchant load ' + l.value + ' dictionary')
+    logging.info('Correctors are ready!')
+
+class CorrectorHandler():
+    def __init__(self, language:CorrectorLanguageEnum):
+        if language not in Correctors.keys():
+            raise Exception('No corrector found for ' + language.name + ' language')
+        self._corrector = Correctors[language]
+    def __correct(self, word):
+        '''Correct the passed word with the firs found'''
+        try:
+            return self._corrector.suggest(word)[0]
+        except:
+            return word
+    def Handle(self, word):
+        '''Return the word if is correct, otherwise the corrected word'''
+        if self._corrector.check(word):
+            return word
+        else:
+            return self.__correct(word)
